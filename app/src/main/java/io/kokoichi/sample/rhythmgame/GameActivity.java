8c9a184fdbeb7fc73d75584800b89356a731fe2f
kokoichi <tt.300607@gmail.com>
2021-08-19 18:36:03 +0900

    add: setup for game

--- app/src/main/java/io/kokoichi/sample/rhythmgame/GameActivity.java
+++ app/src/main/java/io/kokoichi/sample/rhythmgame/GameActivity.java
@@ -1,4 +1,23 @@
 package io.kokoichi.sample.rhythmgame;
 
-public class GameActivity {
+import android.graphics.Point;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+
+        gameView = new GameView(this, point.x, point.y);
+
+        setContentView(gameView);
+    }
 }
--- app/src/main/java/io/kokoichi/sample/rhythmgame/GameView.java
+++ app/src/main/java/io/kokoichi/sample/rhythmgame/GameView.java
@@ -1,4 +1,26 @@
 package io.kokoichi.sample.rhythmgame;
 
-public class GameView {
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements Runnable {
+
+    public GameView(GameActivity activity, int screenX, int screenY){
+        super(activity);
+    }
+
+    @Override
+    public void run() {
+        update();
+        draw();
+        sleep();
+    }
+
+    private void sleep() {
+    }
+
+    private void update() {
+    }
+
+    private void draw() {
+    }
 }
