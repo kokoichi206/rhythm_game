5b541a4aa44a83ec70843c3aba7524e517d2bdc9
kokoichi <tt.300607@gmail.com>
2021-08-23 00:30:57 +0900

    feat: CRUD for play record

--- app/src/main/java/io/kokoichi/sample/rhythmgame/DatabaseHelper.java
+++ app/src/main/java/io/kokoichi/sample/rhythmgame/DatabaseHelper.java
@@ -1,2 +1,49 @@
-package io.kokoichi.sample.rhythmgame;public class DatabaseHelper {
+package io.kokoichi.sample.rhythmgame;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.ContactsContract;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version!!
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "RhythmGame.db";
+
+    public static final String TABLE_NAME = "PlayRecords";
+    public static final String _ID = "id";
+    public static final String MUSIC_NAME = "name";
+    public static final String COMBO = "combo";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + DatabaseHelper.TABLE_NAME + " (" +
+                    _ID + " INTEGER PRIMARY KEY," +
+                    MUSIC_NAME + " TEXT," +
+                    COMBO + " INTEGER)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
 }
--- app/src/main/java/io/kokoichi/sample/rhythmgame/MainActivity.java
+++ app/src/main/java/io/kokoichi/sample/rhythmgame/MainActivity.java
@@ -2,8 +2,11 @@ package io.kokoichi.sample.rhythmgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, GameActivity.class));
         });
 
+        deleteRecord("kimigayo");
+        insertCombo("kimigayo", 33);
         // Get and Display the max combo
         int max_combo = getHighCombo();
         String message = String.format(getString(R.string.display_high_score), max_combo);
@@ -39,7 +44,64 @@ public class MainActivity extends AppCompatActivity {
      * @return
      */
     private int getHighCombo() {
-//        dbHelper.
-        return 6;
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+//        Cursor c = db.rawQuery("select * from table where column = ?",new String[]{"data"});
+        int combo = 0;
+        Cursor cursor = null;
+        try {
+            cursor = db.rawQuery("select * from PlayRecords where name = ?",new String[]{"kimigayo"});
+            int id[] = new int[cursor.getCount()];
+            int i = 0;
+            if (cursor.getCount() > 0)
+            {
+                cursor.moveToFirst();
+
+                int idx = cursor.getColumnIndex("combo");
+                combo = Integer.parseInt(cursor.getString(idx));
+            } else {
+                combo = -1;
+            }
+        }
+        finally {
+            if(cursor != null) {
+                cursor.close();
+            }
+        }
+
+        return combo;
+    }
+
+    /**
+     *
+     * @param music
+     * @param combo
+     * @return the number of lines (if not found, return -1)
+     */
+    private long insertCombo(String music, int combo) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("name", music);
+        values.put("combo", combo);
+
+        return db.insert(dbHelper.TABLE_NAME, null, values);
+    }
+
+    private boolean deleteRecord(String music) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        return db.delete(dbHelper.TABLE_NAME, "name = '" + music + "'", null) == 1;
+    }
+
+    private long updateRecord(String music, int combo) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("name", music);
+        values.put("combo", combo);
+
+        String whereClause = "name = '" + music + "'";
+
+        return db.update(dbHelper.TABLE_NAME, values, whereClause,null);
     }
 }
