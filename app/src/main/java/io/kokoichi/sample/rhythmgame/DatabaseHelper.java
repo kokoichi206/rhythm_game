3a61152ec1e41cdebce4dcba4794ef122c00a455
kokoichi <tt.300607@gmail.com>
2021-08-22 11:17:58 +0900

    style: display the max combo

---
+++ app/src/main/java/io/kokoichi/sample/rhythmgame/DatabaseHelper.java
@@ -0,0 +1,2 @@
+package io.kokoichi.sample.rhythmgame;public class DatabaseHelper {
+}
--- app/src/main/java/io/kokoichi/sample/rhythmgame/MainActivity.java
+++ app/src/main/java/io/kokoichi/sample/rhythmgame/MainActivity.java
@@ -2,10 +2,14 @@ package io.kokoichi.sample.rhythmgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseHelper dbHelper = new DatabaseHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,27 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.play).setOnClickListener((view) -> {
             startActivity(new Intent(MainActivity.this, GameActivity.class));
         });
-        
+
+        // Get and Display the max combo
+        int max_combo = getHighCombo();
+        String message = String.format(getString(R.string.display_high_score), max_combo);
+        TextView combo_text = findViewById(R.id.display_high_score);
+        combo_text.setText(message);
+    }
+
+    @Override
+    protected void onDestroy() {
+        dbHelper.close();
+        super.onDestroy();
+    }
+
+    /**
+     * Get music high score (combo) from local db
+     *
+     * @return
+     */
+    private int getHighCombo() {
+//        dbHelper.
+        return 6;
     }
 }
