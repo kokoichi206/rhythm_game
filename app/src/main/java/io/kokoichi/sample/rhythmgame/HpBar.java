8a6d849c49ce24cff6f3cf63c3816a626a6f31a9
kokoichi <tt.300607@gmail.com>
2021-08-25 07:55:46 +0900

    style: display hp status at the top left (#37)

--- app/src/main/java/io/kokoichi/sample/rhythmgame/HpBar.java
+++ app/src/main/java/io/kokoichi/sample/rhythmgame/HpBar.java
@@ -1,4 +1,34 @@
 package io.kokoichi.sample.rhythmgame;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+import static io.kokoichi.sample.rhythmgame.GameView.screenRatioX;
+import static io.kokoichi.sample.rhythmgame.GameView.screenRatioY;
+
 public class HpBar {
+
+    int x = 100;
+    int y = 80;
+    int width = 400;
+    int height = 80;
+    Bitmap hp_bar, max_hp_bar;
+    int max_hp;
+
+    HpBar (Resources res) {
+
+        hp_bar = BitmapFactory.decodeResource(res, R.drawable.hp_bar);
+        max_hp_bar = BitmapFactory.decodeResource(res, R.drawable.max_hp_bar);
+
+        x = (int) (x * screenRatioX);
+        y = (int) (y * screenRatioY);
+
+        width = (int) (width * screenRatioX);
+        height = (int) (height * screenRatioY);
+
+        hp_bar = Bitmap.createScaledBitmap(hp_bar, width, height, false);
+        max_hp_bar = Bitmap.createScaledBitmap(max_hp_bar, width, height, false);
+    }
 }
