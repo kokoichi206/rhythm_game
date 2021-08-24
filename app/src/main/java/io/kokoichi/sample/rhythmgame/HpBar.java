3a5813b8be6990262cb4e8de154562abbc958d47
kokoichi <tt.300607@gmail.com>
2021-08-25 08:50:42 +0900

    feat: define the max hp (#38)

--- app/src/main/java/io/kokoichi/sample/rhythmgame/HpBar.java
+++ app/src/main/java/io/kokoichi/sample/rhythmgame/HpBar.java
@@ -4,6 +4,7 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
+import android.util.Log;
 
 import static io.kokoichi.sample.rhythmgame.GameView.screenRatioX;
 import static io.kokoichi.sample.rhythmgame.GameView.screenRatioY;
@@ -13,9 +14,11 @@ public class HpBar {
     int x = 100;
     int y = 80;
     int width = 400;
+    int current_width;
     int height = 80;
     Bitmap hp_bar, max_hp_bar;
     int max_hp;
+    int current_hp;
 
     HpBar (Resources res) {
 
@@ -30,5 +33,12 @@ public class HpBar {
 
         hp_bar = Bitmap.createScaledBitmap(hp_bar, width, height, false);
         max_hp_bar = Bitmap.createScaledBitmap(max_hp_bar, width, height, false);
+    }
+
+    void update() {
+
+        int new_width = width * current_hp / max_hp;
+        hp_bar = Bitmap.createScaledBitmap(hp_bar, new_width, height, false);
+
     }
 }
