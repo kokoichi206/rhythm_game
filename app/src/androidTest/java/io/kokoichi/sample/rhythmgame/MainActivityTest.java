99ec6af48f62b8cdf8bbfa33d3bc84e50d7d2fd6
kokoichi <tt.300607@gmail.com>
2021-08-26 07:45:09 +0900

    add: test file

---
+++ app/src/androidTest/java/io/kokoichi/sample/rhythmgame/MainActivityTest.java
@@ -0,0 +1,30 @@
+package io.kokoichi.sample.rhythmgame;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onActivityResult() {
+    }
+
+    @Test
+    public void savePref() {
+    }
+
+    @Test
+    public void onDestroy() {
+    }
+}
