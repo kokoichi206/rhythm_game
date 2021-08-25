188c00ce0c79a0bd1dcea41431850a27c6ee0c18
kokoichi <tt.300607@gmail.com>
2021-08-26 08:22:51 +0900

    test: launch test (#44)

--- app/src/androidTest/java/io/kokoichi/sample/rhythmgame/MainActivityTest.java
+++ app/src/androidTest/java/io/kokoichi/sample/rhythmgame/MainActivityTest.java
@@ -1,30 +1,55 @@
 package io.kokoichi.sample.rhythmgame;
 
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 public class MainActivityTest {
 
+    // To test the activity
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    private MainActivity mActivity = null;
+
+    private int[] ids = {
+            R.id.background_home,
+            R.id.play,
+            R.id.display_high_score,
+            R.id.max_hp_bar,
+            R.id.hp_bar,
+            R.id.exp_txt,
+            R.id.display_rank_num,
+            R.id.display_rank_txt,
+    };
+
     @Before
     public void setUp() throws Exception {
+
+        mActivity = mActivityTestRule.getActivity();
+
+    }
+
+    @Test
+    public void testLaunch() {
+
+        for (int id: ids) {
+            View view = mActivity.findViewById(id);
+            assertNotNull(view);
+        }
     }
 
     @After
     public void tearDown() throws Exception {
-    }
 
-    @Test
-    public void onActivityResult() {
-    }
-
-    @Test
-    public void savePref() {
-    }
-
-    @Test
-    public void onDestroy() {
+        mActivity = null;
     }
 }
