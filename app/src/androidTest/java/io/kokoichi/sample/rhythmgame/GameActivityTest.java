35e6c80aa71a92d27be6d8bccf1c392787ee54da
kokoichi <tt.300607@gmail.com>
2021-08-26 22:19:18 +0900

    test: gameActivity (#45)

--- app/src/androidTest/java/io/kokoichi/sample/rhythmgame/GameActivityTest.java
+++ app/src/androidTest/java/io/kokoichi/sample/rhythmgame/GameActivityTest.java
@@ -1,14 +1,39 @@
 package io.kokoichi.sample.rhythmgame;
 
+import androidx.test.rule.ActivityTestRule;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 public class GameActivityTest {
 
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityMainTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    private MainActivity mActivityMain = null;
+
+    @Rule
+    public ActivityTestRule<GameActivity> mActivityGameTestRule = new ActivityTestRule<GameActivity>(GameActivity.class);
+
+    private GameActivity mActivityGame = null;
+
     @Before
     public void setUp() throws Exception {
+
+        mActivityMain = mActivityMainTestRule.getActivity();
+        mActivityGame = mActivityGameTestRule.getActivity();
+
+    }
+
+    @Test
+    public void testInit() {
+
+        assertNotNull(mActivityGame.gameView);
+
     }
 
     @After
