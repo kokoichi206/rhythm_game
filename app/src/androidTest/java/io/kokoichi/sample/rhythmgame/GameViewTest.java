a0596b3b320e81791bdba9a5ed4e6efcf3a34bb3
kokoichi <tt.300607@gmail.com>
2021-08-26 22:28:16 +0900

    add: test file (#46)

---
+++ app/src/androidTest/java/io/kokoichi/sample/rhythmgame/GameViewTest.java
@@ -0,0 +1,17 @@
+package io.kokoichi.sample.rhythmgame;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class GameViewTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
