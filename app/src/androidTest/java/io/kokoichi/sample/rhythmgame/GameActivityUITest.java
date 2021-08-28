51056c05e2d7251c59870cb2f24c4e2c9ade7bd8
kokoichi <tt.300607@gmail.com>
2021-08-29 08:44:14 +0900

    test: setup (#49)

--- app/src/androidTest/java/io/kokoichi/sample/rhythmgame/GameActivityUITest.java
+++ app/src/androidTest/java/io/kokoichi/sample/rhythmgame/GameActivityUITest.java
@@ -1,4 +1,27 @@
 package io.kokoichi.sample.rhythmgame;
 
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+
 public class GameActivityUITest {
+
+    @Rule
+    public ActivityTestRule<GameActivity> mActivityGameTestRule = new ActivityTestRule<GameActivity>(GameActivity.class);
+
+    private GameActivity mActivityGame = null;
+
+    @Before
+    public void setUp() {
+        mActivityGame = mActivityGameTestRule.getActivity();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivityGame = null;
+    }
+
+
 }
