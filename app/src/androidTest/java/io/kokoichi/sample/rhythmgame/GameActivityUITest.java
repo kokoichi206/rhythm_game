213c70874ba2961c5a9f0efde4389a1fe269b428
kokoichi <tt.300607@gmail.com>
2021-08-29 10:30:56 +0900

    test: pause button (#49)

--- app/src/androidTest/java/io/kokoichi/sample/rhythmgame/GameActivityUITest.java
+++ app/src/androidTest/java/io/kokoichi/sample/rhythmgame/GameActivityUITest.java
@@ -5,10 +5,16 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.GeneralClickAction;
+import androidx.test.espresso.action.Press;
+import androidx.test.espresso.action.Tap;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.rule.ActivityTestRule;
 
@@ -33,6 +39,53 @@ public class GameActivityUITest {
 
     }
 
+    @Test
+    public void testPauseButton() {
+
+        // Click the pause button
+        Button button = mActivityGame.gameView.button;
+        onView(isRoot()).perform(clickXY(button.startX, button.startY));
+
+        // Check if the pause dialog is showing
+        onView(withText(R.string.pause_dialog_message))
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testContinueButtonInPauseDialog() {
+
+        // Click the pause button
+        Button button = mActivityGame.gameView.button;
+        onView(isRoot()).perform(clickXY(button.startX, button.startY));
+
+        // Click the continue button
+        onView(withText(R.string.pause_dialog_continue))
+                .inRoot(isDialog())
+                .perform(click());
+
+        // Check if the activity is not destroying
+        assertFalse(mActivityGameTestRule.getActivity().isFinishing());
+    }
+
+    @Test
+    public void testQuitButtonInPauseDialog() {
+
+        // Click the pause button
+        Button button = mActivityGame.gameView.button;
+        onView(isRoot()).perform(clickXY(button.startX, button.startY));
+
+        // Click the quit button
+        onView(withText(R.string.pause_dialog_quit))
+                .inRoot(isDialog())
+                .perform(click());
+
+        // Check if the activity is destroying
+        assertTrue(mActivityGame.isFinishing());
+
+        // The music is stopped
+        assertFalse(mActivityGame.gameView.myPlayer.player.isPlaying());
+    }
 
     @Test
     public void testDeadDialog() {
@@ -45,12 +98,12 @@ public class GameActivityUITest {
                 .inRoot(isDialog())
                 .check(matches(isDisplayed()));
 
-        // Click the ok button in
+        // Click the ok button in the dialog
         onView(withText(R.string.dead_dialog_ok))
                 .perform(click());
 
         // Check if the GameActivity finishes
-        assertTrue(mActivityGameTestRule.getActivity().isFinishing());
+        assertTrue(mActivityGame.isFinishing());
 
         // The music is stopped
         assertFalse(mActivityGame.gameView.myPlayer.player.isPlaying());
@@ -63,5 +116,24 @@ public class GameActivityUITest {
         Intents.release();
 
     }
-    
+
+    //
+    // Custom click function using (x, y) to click
+    //
+    public static ViewAction clickXY(final int x, final int y) {
+        return new GeneralClickAction(
+                Tap.SINGLE,
+                view -> {
+
+                    final int[] screenPos = new int[2];
+                    view.getLocationOnScreen(screenPos);
+
+                    final float screenX = screenPos[0] + x;
+                    final float screenY = screenPos[1] + y;
+                    float[] coordinates = {screenX, screenY};
+
+                    return coordinates;
+                },
+                Press.FINGER);
+    }
 }
